80674368f547b17e78d52ed0c9abf3a1d1cd7f7e
jay <jay241093@gmail.com>
2023-08-14 14:17:18 +0100

    task 2, added test

--- __tests__/topics.test.js
+++ __tests__/topics.test.js
@@ -1,28 +1,42 @@
-const app = require('../db/seeds/app');
-const request = require('supertest');
-const connection = require('../db/connection');
-const seed = require('../db/seeds/seed');
+const app = require("../db/seeds/app");
+const request = require("supertest");
+const connection = require("../db/connection");
+const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data/index");
 
 afterAll(() => {
-    return connection.end();
+  return connection.end();
 });
 beforeEach(() => {
-    return seed(data);
+  return seed(data);
 });
 
-describe('app', () => {
-    describe('api/healthcheck', () => {
-        test("returns 200 status code", () => {
-            return request(app).get("/api/healthcheck").expect(200);
+describe("app", () => {
+  describe("api/healthcheck", () => {
+    test("returns 200 status code", () => {
+      return request(app).get("/api/healthcheck").expect(200);
+    });
+  });
+  describe("api/topics", () => {
+    test("GET: 200 sends an array of topics with properties of slug and description", () => {
+      return request(app).get("/api/topics").expect(200);
+    });
+    test("Responds with an object with slug and description properties", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toEqual({
+            topics: [
+              {
+                slug: "mitch",
+                description: "The man, the Mitch, the legend",
+              },
+              { slug: "cats", description: "Not dogs" },
+              { slug: "paper", description: "what books are made of" },
+            ],
+          });
         });
     });
-    describe('api/topics', () => {
-        test('GET: 200 sends an array of topics with properties of slug and description', () => {
-            return request(app)
-            .get('/api/topics')
-            .expect(200)
-        });
-    });
-})
-
+  });
+});
